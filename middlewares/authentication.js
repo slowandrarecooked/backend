54b71f0d8f89b2f632a2437f7670c95768f9b6e1
slowandrarecooked <airbendervineeth@gmail.com>
2023-09-04 19:52:43 +0530

    v3

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 
 const authentication = (req, res, next) => {
-  const token = req.headers.authorization.split(" ")[1];
+  const token = req.headers.authorization?.split(" ")[1];
 
   jwt.verify(token, process.env.SECRET, (err, decoded) => {
     if (err) res.send("auth failed");
